a66e30268b94604a712883f32354127b1dfb47b4
vanshita03 <144714349+vanshita03@users.noreply.github.com>
2024-08-01 00:29:21 +0530

    Add files via upload

---
+++ port/index.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal(".hero" ,{duration:7000,opacity:0.5,reset:true,mobile: true,easing:"ease-out"} );
+// ScrollReveal().reveal(".hero-content h1",{interval:6000});
+ScrollReveal().reveal("#about",{delay:200,reset:true,mobile: true});
+ScrollReveal().reveal("#portfolio",{delay:1000,reset:true,mobile: true});
+ScrollReveal().reveal("#contact",{delay:1000,reset:true,mobile: true});
